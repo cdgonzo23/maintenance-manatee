3a95ab0460b4eb4b68715226e692c6fd50ac411f
Chesney Julian <chesney.julian724@gmail.com>
2023-07-31 22:03:18 -0400

    add vehicle adding, updating, and deleting routes

--- controllers/api/vehicle-routes.js
+++ controllers/api/vehicle-routes.js
@@ -1,4 +1,53 @@
 const router = require('express').Router();
+const { Vehicle } = require('../../models');
+const authorize = require('../../utils/authorize');
 
+router.post('/add-vehicle', authorize, async (req, res) => {
+    try {
+        const newVehicleData = await Vehicle.create({
+            manufacturer: req.body.manufacturer,
+            modelName: req.body.modelName,
+            year: req.body.year,
+            color: req.body.color,
+            nickname: req.body.nickname,
+            userId: res.session.userId
+        });
+        res.status(200).json(newVehicleData)
+    } catch (err) {
+        res.status(500).json(err);
+    };
+});
+
+router.put('/:id', authorize, async (req, res) => {
+    try {
+         const vehicle = await Vehicle.update(
+       {
+        manufacturer: req.body.manufacturer,
+        modelName: req.body.modelName,
+        year: req.body.year,
+        color: req.body.color,
+        nickname: req.body.nickname,
+    }, 
+    {
+        where: {
+            id: req.params.id
+        }
+    });
+        res.status(200).json(vehicle);
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
+
+router.delete('/delete/:id', authorize, async (req, res) => {
+    try {
+    const deletedVehicle = await Vehicle.destroy({where: {
+        id: req.params.id
+    }});
+    res.status(200).json({message: 'Vehicle deleted'});
+    } catch (err) {
+        res.status(500).json(err);
+    };
+})
 
 module.exports = router;
