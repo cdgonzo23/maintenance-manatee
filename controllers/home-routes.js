52200c005c96351f0374ffb95fe49bd9bae14c78
Chesney Julian <chesney.julian724@gmail.com>
2023-08-01 20:00:45 -0400

    create routes to get all posts for one vehicle and to edit a post

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -15,11 +15,33 @@ router.get('/', async (req, res) => {
         ],
         });
         const user = userData.get({ plain: true });
-        if (!user) res.status(404).json({message: "No user found."});
+        if (!user) res.status(404).json({message: 'No user found.'});
         res.render('homepage', { user })
     } catch (err) {
         res.status(500).json(err);
     };
+})
+
+router.get('/vehicle/:id', authorize, async (req, res) => {
+  try {
+    const vehicleData = await Vehicle.findOne({
+      where: {
+        id: req.params.id
+      }
+    },
+    {
+      include: [
+        {
+          model: Post
+        }
+      ]
+    });
+    const vehicle = vehicleData.get({ plain: true });
+    if (!vehicle) res.status(404).json({message: 'No vehicle found.'});
+    res.render('maintenance-posts', { vehicle });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 })
 
 router.get('/login', (req, res) => {
