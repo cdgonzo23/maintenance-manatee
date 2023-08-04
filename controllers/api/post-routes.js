b7ab138dc181bbe8b070ed9e7f9608fc5fc9a769
Chesney Julian <chesney.julian724@gmail.com>
2023-08-03 20:21:52 -0400

    make sure req.session data persists throughout all home routes, remove unnecessary authorize function in api routes; add route to delete post and edit route to create post

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,15 +1,14 @@
-const authorize = require('../../utils/authorize');
 const router = require('express').Router();
 const { Vehicle, Post } = require('../../models');
 
-router.post('/:id', authorize, async (req, res) => {
+router.post('/add-post', async (req, res) => {
     try {
         const newPostData = await Post.create({
             title: req.body.title,
             content: req.body.content,
             cost: req.body.cost,
             dateOfMaintenance: req.body.dateOfMaintenance,
-            vehicleId: req.params.id
+            vehicleId
         })
         res.status(200).json(newPostData);
     } catch (err) {
@@ -17,7 +16,7 @@ router.post('/:id', authorize, async (req, res) => {
     };
 });
 
-router.put('/edit/:id', authorize, async (req, res) => {
+router.put('/edit/:id', async (req, res) => {
     try {
         const editPostData = await Post.update({
             title: req.body.title,
@@ -32,6 +31,19 @@ router.put('/edit/:id', authorize, async (req, res) => {
         });
         if (!editPostData) res.status(500).json({message: 'Failed to edit post'});
         res.status(200).json(editPostData);
+    } catch (err) {
+        res.status(500).json(err);
+    };
+});
+
+router.delete('/delete/:id', async (req, res) => {
+    try {
+        const deletedPost = await Post.delete({
+            where: {
+                id: req.params.id
+            }
+        });
+        res.status(200).json({message: 'Post Deleted'});
     } catch (err) {
         res.status(500).json(err);
     };
