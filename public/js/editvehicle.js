9d2dd8840b8ba61754beb202df03cff7d9db0770
Zgibbs58 <zacharywgibbs@gmail.com>
2023-08-03 21:34:08 -0500

    Added edit-vehicle.handlebars and updated editvehicle.js.

--- public/js/editvehicle.js
+++ public/js/editvehicle.js
@@ -1,30 +1,24 @@
 // function to handle editing a vehicle
-
 const editVehicleFormHandler = async (event) => {
-    event.preventDefault();
+  event.preventDefault();
+  const manufacturer = document.querySelector("#manufacturer").value.trim();
+  const modelName = document.querySelector("#model").value.trim();
+  const year = document.querySelector("#year").value.trim();
+  const color = document.querySelector("#color").value.trim();
+  const nickname = document.querySelector("#nickname").value.trim();
+  // TODO: figure out how we're pulling in vehicleId for a given vehicle
+  const vehicleId = document.querySelector("#edit-vehicle-form").getAttribute("data-vehicle-id");
 
-    const manufacturer = document.querySelector('#manufacturer').value.trim();
-    const modelName = document.querySelector('#model').value.trim();
-    const year = document.querySelector('#year').value.trim();
-    const color = document.querySelector('#color').value.trim();
-    const nickname = document.querySelector('#nickname').value.trim();
-    // TODO: figure out how we're pulling in vehicleId for a given vehicle
-    const vehicleId = document.querySelector('#vehicleId').value.trim();
-
-    if (manufacturer && modelName && year && color && nickname && vehicleId) {
-        const response = await fetch(`/api/vehicle/${vehicleId}`, {
-            method: 'PUT',
-            body: JSON.stringify({ manufacturer, modelName, year, color, nickname }),
-        });
-
-        if (response.ok) {
-            document.location.replace('/');
-        } else {
-            alert('failed to edit vehicle');
-        }
+  if (manufacturer && modelName && year && color && nickname && vehicleId) {
+    const response = await fetch(`/api/vehicle/${vehicleId}`, {
+      method: "PUT",
+      body: JSON.stringify({ manufacturer, modelName, year, color, nickname }),
+    });
+    if (response.ok) {
+      document.location.replace("/homepage");
+    } else {
+      alert("failed to edit vehicle");
     }
+  }
 };
-
-document
-    .querySelector('#editvehicle-form')
-    .addEventListener('submit', editVehicleFormHandler);
+document.querySelector("#editvehicle-form").addEventListener("submit", editVehicleFormHandler);
