1004d2b73989dbd919d9ecfb81d23f473b98861c
Zgibbs58 <zacharywgibbs@gmail.com>
2023-08-08 12:16:25 -0500

    Added helper function to sort posts by newest date.

--- utils/helpers.js
+++ utils/helpers.js
@@ -2,4 +2,7 @@ module.exports = {
   format_date: (date) => {
     return `${new Date(date).getUTCMonth() + 1}/${new Date(date).getUTCDate()}/${new Date(date).getUTCFullYear()}`;
   },
+  sortPostDates: (posts) => {
+    posts.sort(({ dateOfMaintenance: a }, { dateOfMaintenance: b }) => (a < b ? 1 : a > b ? -1 : 0));
+  },
 };
